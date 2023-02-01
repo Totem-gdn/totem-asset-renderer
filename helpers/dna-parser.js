683c556f65e9e7c74502769261f2ab7042907f8b
illiadimension <ilya.nikulin@it-dimension.com>
2023-02-01 13:41:18 +0200

    add reverse api url

--- helpers/dna-parser.js
+++ helpers/dna-parser.js
@@ -4,6 +4,7 @@ const { DNAParser, ContractHandler } = require('totem-dna-parser')
 class NFT {
   constructor() {
     this.ApiURL = process.env.API_URL;
+    this.ReverseApiURL = process.env.RESERVE_API_URL;
     this.Contracts = {
       avatar: process.env.AVATAR_CONTRACT,
       item: process.env.ITEM_CONTRACT,
@@ -20,8 +21,14 @@ class NFT {
         defaultJson = DNAParser.defaultItemJson;
       }
 
-      const contractHandler = new ContractHandler(this.ApiURL, this.Contracts[type]);
-      const dna = await contractHandler.getDNA(id);
+      let dna;
+      try {
+        const contractHandler = new ContractHandler(this.ApiURL, this.Contracts[type]);
+        dna = await contractHandler.getDNA(id);
+      } catch (error) {
+        const contractHandler = new ContractHandler(this.ReverseApiURL, this.Contracts[type]);
+        dna = await contractHandler.getDNA(id);
+      }
       const parser = new DNAParser(defaultJson, dna);
       const properties = parser.getFilterPropertiesList()
       let jsonProp = {...properties};
